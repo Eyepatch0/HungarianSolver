9ca3aa3da67a4e13e3b1f165137ff0523b99dc9e
Syed Mohammed <cocsmdkillet@gmail.com>
2023-03-14 21:02:20 +0530

    Accepting user Input and storing it in 2d Matrix

--- HungarianSolver/src/components/Solve.tsx
+++ HungarianSolver/src/components/Solve.tsx
@@ -1,23 +1,25 @@
 import { useState } from "react";
 
-import InputMatrixComponent from "./InputMatrixComponent";
+import InputMatrix from "./InputMatrix";
+import SolvedMatrix from "./SolvedMatrix";
 
 const Solve = () => {
-  const [size, setSize] = useState("");
+  const [size, setSize] = useState<number>(3);
   const [matrix, setMatrix] = useState<number[][]>([]);
 
   const sizeValidator: (size: number) => boolean = (size) => {
-    if (size > 10) {
-      alert("Maximum size allowed is 10");
-      setMatrix([]);
-      return false;
-    } else if (size < 3) {
-      alert("Minimum size is 3");
-      setMatrix([]);
-      return false;
+    switch (true) {
+      case size > 10:
+        alert("Maximum size allowed is 10.");
+        return false;
+      case size < 3:
+        alert("Minimum size is 3.");
+        return false;
+      default:
+        return true;
     }
-    return true;
   };
+
   const matrixFiller = (size: number) => {
     if (!sizeValidator(size)) return;
     const newMatrix: number[][] = Array.from(Array(size), () =>
@@ -26,43 +28,52 @@ const Solve = () => {
     setMatrix(newMatrix);
   };
 
+  const matrixUpdater = (row: number, col: number, value: number) => {
+    const newMatrix = [...matrix];
+    newMatrix[row][col] = value;
+    setMatrix(newMatrix);
+  };
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-400 to-slate-50 flex justify-center items-center dark:bg-gradient-to-br dark:from-slate-900 dark:to-slate-700">
-      <div className="container border border-emerald-500 md:border-4 rounded-lg min-h-[70vh] flex flex-col md:flex-row font-inter text-slate-900">
-        <div className="basis-1/2 border-r flex flex-col justify-between">
-          <div className="flex items-center border flex-col md:flex-row basis-1/4">
-            <div className="flex flex-col text-md font-semibold p-5 border basis-3/4">
+      <div className="container border border-emerald-500 dark:border-emerald-900 md:border-4 rounded-lg min-h-[70vh] flex flex-col xl:flex-row font-inter text-slate-900 bg-gradient-to-bl from-emerald-400 via-green-200 to-emerald-400 dark:bg-gradient-to-bl dark:from-green-800 dark:via-emerald-400 dark:to-green-800 dark:text-slate-800">
+        <div className="basis-1/2 sm:border-b md:border-b-0 md:border-r flex flex-col justify-between">
+          <div className="basis-1/4 flex items-center flex-col md:flex-row md:justify-center">
+            <div className="flex flex-col text-md font-semibold p-5">
               <label htmlFor="matrixSize">Enter the Size of the Matrix</label>
               <input
                 type="number"
                 id="matrixSize"
                 value={size}
-                onChange={(e) => setSize(e.target.value)}
+                onChange={(e) => setSize(parseInt(e.target.value))}
                 max={10}
                 maxLength={2}
-                min={1}
-                minLength={1}
-                className="border border-emerald-500 rounded-md px-2 w-9 focus:ring-1 focus:ring-emerald-600 focus:outline-none my-2 matrixSize"
+                accept="number"
+                className="border border-emerald-500 rounded-md px-1 w-9 focus:ring-1 focus:ring-emerald-600 focus:outline-none my-2 matrixSize dark:bg-slate-700 dark:text-slate-100"
               />
               <p className="italic text-sm font-light">
                 Maximum size allowed is 10.
               </p>
               <p className="italic text-sm font-light">Minimum size is 3.</p>
             </div>
-            <div className="p-5 border basis-1/4 h-full flex justify-center items-center">
+            <div className="p-5 basis-1/4 h-full flex justify-center items-center">
               <button
-                className="border border-emerald-700 rounded p-1 text-slate-100 bg-emerald-500 hover:bg-emerald-600"
-                onClick={() => matrixFiller(parseInt(size))}
+                className="border border-emerald-700 rounded p-1 text-slate-100 bg-emerald-500 hover:bg-emerald-600 dark:bg-gradient-to-r dark:from-slate-700 dark:to-slate-800 hover:scale-105 transition ease-in-out"
+                onClick={() => matrixFiller(size)}
               >
                 Submit
               </button>
             </div>
           </div>
-          <div className="flex justify-center items-center basis-3/4 h-full">
-            {matrix.length > 0 && <InputMatrixComponent matrix={matrix} />}
+          <div className="flex justify-center items-center basis-3/4 h-full mx-2">
+            {matrix.length > 0 && (
+              <InputMatrix matrix={matrix} matrixUpdater={matrixUpdater} />
+            )}
           </div>
         </div>
-        <div className="basis-1/2 border-l">Solve</div>
+        <div className="basis-1/2 border-t md:border-l">
+          {matrix.length > 0 && <SolvedMatrix />}
+        </div>
       </div>
     </div>
   );
---
+++ HungarianSolver/src/components/SolvedMatrix.tsx
@@ -0,0 +1,5 @@
+const SovledMatrix = () => {
+  return <>Solved</>;
+};
+
+export default SovledMatrix;
